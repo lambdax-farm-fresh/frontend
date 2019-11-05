95321abe26eff6d7c2a5f384604e49f430c4946f
Steve Smodish <smodish@hotmail.com>
2019-11-05 18:41:10 -0500

    added react-hook-form built CreateLocation form component currently all fields required until we get it hooked up

--- src/components/Locations/CreateLocation.js
+++ src/components/Locations/CreateLocation.js
@@ -1,9 +1,56 @@
 import React from "react";
+import useForm from "react-hook-form";
 
 const CreateLocation = props => {
+  const { register, handleSubmit, reset, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <>
+      <hr />
+
       <h1>Create Location</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="streetNumber">Street Number</label>
+        <input name="streetNumber" ref={register({ required: true })} />
+        {errors.streetNumber && errors.streetNumber.type === "required" && (
+          <p>This is required</p>
+        )}
+        <br />
+
+        <label htmlFor="streetName">Street Name</label>
+        <input name="streetName" ref={register({ required: true })} />
+        {errors.streetName && errors.streetName.type === "required" && (
+          <p>This is required</p>
+        )}
+        <br />
+
+        <label htmlFor="city">City</label>
+        <input name="city" ref={register({ required: true })} />
+        {errors.city && errors.city.type === "required" && (
+          <p>This is required</p>
+        )}
+        <br />
+
+        <label htmlFor="state">State</label>
+        <input name="state" ref={register({ required: true })} />
+        {errors.state && errors.state.type === "required" && (
+          <p>This is required</p>
+        )}
+        <br />
+
+        <label htmlFor="zip">Zipcode</label>
+        <input name="zip" ref={register({ required: true })} />
+        {errors.zip && errors.zip.type === "required" && (
+          <p>This is required</p>
+        )}
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+      <hr />
     </>
   );
 };
