aeabfe220981bdd2db69e6c86e3e071a73b7bf5c
Steve Smodish <smodish@hotmail.com>
2019-11-05 19:08:21 -0500

    created fake location data with mockaroo put data in testLocations.js setup map in Locations component fleshed out Location component

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+import testLocations from "./testLocations";
 
 import Location from "./Location";
 import CreateLocation from "./CreateLocation";
 
 const Locations = props => {
+  const [locations, setLocations] = useState(testLocations);
+
   return (
     <>
-      <div>List of Locations</div>
-      <div>Map over locations</div>
       <div>
-        <Location />
+        <hr />
         <CreateLocation />
+
+        {locations.map(location => {
+          return <Location data={location} />;
+        })}
+        <hr />
       </div>
     </>
   );
