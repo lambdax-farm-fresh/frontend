2128b72cffdba53973df96d101c6fff695b6f20b
Adam Mathieson <mathieson1222@gmail.com>
2020-01-23 21:25:18 -0800

    adds test to dev dependancies; adds AddLocation test suite and render test

---
+++ src/components/Layout/Dashboard/Farm/_tests_/AddLocation.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+
+import AddLocation from '../Locations/AddLocation'
+
+afterEach(() => {
+    cleanup();
+    console.error.mockClear();
+})
+
+// Spy
+console.error = jest.fn();
+
+test('<AddLocation />', () => {
+    render(<AddLocation />)
+    expect(console.error).toHaveBeenCalled()
+})
+
+
